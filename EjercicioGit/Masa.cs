727099246f05fea7ff7c61ac62f45fa5700f7c64
gabbb01 <gabrielfunes2223@gmail.com>
2026-02-14 20:34:36 -0600

    agregando nueva clase

---
+++ EjercicioGit/Masa.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace EjercicioGit
+{
+    public class Masa
+    {
+        private double ValorMasaKg;
+
+        public Masa(double masa)
+        {
+            ValorMasaKg = masa;
+        }
+
+        public void ConversionGramos()
+        {
+            double valor = ValorMasaKg / 1000;
+            Console.WriteLine($"Conversion a Gramos: {valor} g");
+        }
+    }
+
+
+}
--- EjercicioGit/Program.cs
+++ EjercicioGit/Program.cs
@@ -1,6 +1,11 @@
 ﻿using EjercicioGit;
 
 Longitudes longitud1 = new Longitudes(45);
-Console.WriteLine("Longitud1");
+Console.WriteLine("Longitud1 en metros");
 longitud1.ConversionCm();
 longitud1.ConversionKm();
+
+Console.WriteLine("");
+Console.WriteLine("Masa1 en kilogramos");
+Masa masa1 = new Masa(5);
+masa1.ConversionGramos();
