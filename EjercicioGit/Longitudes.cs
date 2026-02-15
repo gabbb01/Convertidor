6e1053c3365312bc8b9b2f7d6fb33c04074c4250
gabbb01 <gabrielfunes2223@gmail.com>
2026-02-14 19:59:11 -0600

    primer commit

---
+++ EjercicioGit/Longitudes.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace EjercicioGit
+{
+    public class Longitudes
+    {
+        private double Metros;
+
+
+        public Longitudes(double metros)
+        {
+            Metros = metros;
+        }
+
+        public void ConversionCm()
+        {
+            double centimetros = Metros * 100;
+            Console.WriteLine($"Total en centimetros: {centimetros}");
+        }
+    }
+}
